6325fbf27ece2e5512454ac04d94bc946b8b75b3
Lyte77 <lighcode77@gmail.com>
2024-09-12 06:36:20 -0700

    Added some webpages

--- solar_website/solar/views.py
+++ solar_website/solar/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def homePage(request):
+    return render(request, 'solar/home.html' )
+
+def aboutPage(request):
+    return render(request, 'solar/about.html')
+
+def servicesPage(request):
+    return render(request, 'solar/services.html')
+
