aa05a4e811e827ef9bbbdb758a61cbfdef3b55d4
Lyte <litecode77@gmail.com>
2024-09-30 06:53:29 -0700

    adjusting settings

--- solar_website/solar_website/settings.py
+++ solar_website/solar_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wp%8&8#7b94&#8(ve!ns#a&f0umhh9&qe3k_eu+=-w!po-ica-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
